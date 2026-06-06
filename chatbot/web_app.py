8495c96f6f4797d9d7c58312daa489cea0105be5
Matz <jyan07093@gmail.com>
2026-06-06 22:53:56 +0530

    Update web_app.py

--- chatbot/web_app.py
+++ chatbot/web_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title("Heat Stress Assistant Chatbot")
+
+user_question = st.text_input("Ask a question")
+
+if user_question:
+    st.write("You asked:", user_question)
