438b0d038cf23b2c17518d3ec73150e95ac15a94
Matz <jyan07093@gmail.com>
2026-06-06 22:01:53 +0530

    Placeholder added for version 2

--- chatbot/app_version2.py
+++ chatbot/app_version2.py
@@ -1,1 +1,5 @@
+# Version 2 Prototype
+# Embedding-based chatbot using semantic similarity
 
+print("Heat Stress Assistant Chatbot - Version 2")
+print("Embedding-based retrieval will be added here.")
